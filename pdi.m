4166e560e2c8ad566a1111e07876c2c3c7bd470e
706684 <matheus.lichtnow@pti.org.br>
2019-04-09 15:25:11 -0300

    Refatorados scripts e máscaras

--- pdi.m
+++ pdi.m
@@ -2,51 +2,49 @@ close all
 clear all
 
 
-IM=imread('unnamed.jpg');
+IM=imread('passaro.jpg');
 
-IM = rgb2gray(IM); %imagem em escala de cinza
-
-M = fftshift(IM); %Aqui ocorre a translação
-
-M = fft2(M)
-
-Ab = real(M);
-
-minimun = min(min(Ab));
-maximun = max(max(Ab));
-
-Ab = (Ab - minimun)./(maximun).*255;
-
-Mask = imread('espectro-mascara.png');
-
-Mask = (Mask)./(65536).*255;
-
-Mask = uint8(Mask);
-
-Ab = immultiply(Mask,Ab);
-
-minimun = min(min(Ab));
-maximun = max(max(Ab));
-
-Ab = (Ab - minimun)./(maximun).*255;
-
-Ab = uint8(Ab);
-
-Z = complex(Ab,imag(M));
-
-Z = ifft2(Z);
-
-Z = ifftshift(Z);
-
-Z = real(Z);
-
-minimun = min(min(Z));
-maximun = max(max(Z));
-
-Z = (Z - minimun)./(maximun).*255;
+IM = rgb2gray(IM); %imagem em escala de cinSaidaa
 
 figure(1);
-imshow(uint8(Z));
+imshow(IM);
+
+IM_FFT2 = fft2(IM);
+
+IM_FFT2 = fftshift(IM_FFT2); %Aqui ocorre a translação
+
+ParteReal = abs(IM_FFT2);
+
+minimun = min(min(ParteReal));
+maximun = max(max(ParteReal));
+
+ParteReal = (ParteReal - minimun)./(maximun-minimun).*255;
+
+figure(2)
+imshow(ParteReal);
+
+Mascara = imread('mascara-full.png');
+Mascara = im2bw(Mascara, 0.8);
+
+figure(3)
+imshow(Mascara);
+
+
+Im_multi = immultiply(Mascara,ParteReal);
+Im_multi = uint8(Im_multi);
+
+Saida = complex(Im_multi,imag(IM_FFT2));
+Saida = ifftshift(Saida);
+Saida = ifft2(Saida);
+Saida = abs(Saida);
+
+minimun = min(min(Saida));
+maximun = max(max(Saida));
+
+Saida = (Saida - minimun)./(maximun-minimun).*255;
+
+figure(4);
+imshow(uint8(Saida));
 
 
 
